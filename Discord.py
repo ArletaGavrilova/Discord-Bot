b33a9b54a9d8a954f3f700b09ece49ab5e79e234
ArletaGavrilova <109897868+ArletaGavrilova@users.noreply.github.com>
2024-05-02 19:25:56 +0300

    Add files via upload

---
+++ Discord.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')    
+
+
+
+bot.run("MTIzMzA3OTk4MzYyNzQ0MDE2OA.G9cKnA.PQaVLaAGa4pN0iTm1Ty7lWJi4m1bEhXqpzxUhs")
